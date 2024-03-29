dbce0ffe7f63c539dd0c2f4d392a1251bf8be968
David Allen <spondicious@me.com>
2024-03-29 12:13:48 +0100

    working inbox page

--- a_inbox/views.py
+++ a_inbox/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import InboxMessage, Conversation
+from .models import *
 
 
 @login_required
 def inbox_view(request):
     conversation = Conversation.objects.first()
+    my_conversations = Conversation.objects.filter(participants=request.user)
     context = {
-        "conversation": conversation
+        "conversation": conversation,
+        "my_conversations": my_conversations
     }
     return render(request, "a_inbox/inbox.html", context)
 
