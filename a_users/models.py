5440dd6cd3a171589327f762271163c7f7406de6
David Allen <spondicious@me.com>
2024-04-03 11:16:54 +0200

    whats gone wrong now?

--- a_users/models.py
+++ a_users/models.py
@@ -34,21 +34,8 @@ class Profile(models.Model):
     # image = models.ImageField(upload_to='avatars/', null=True, blank=True)
     image = ResizedImageField(size=[600, 600], quality=85, upload_to="avatars/", blank=True, null=True)
     
-    displayname = models.CharField(max_length=20, null=True, blank=True)
-    info = models.TextField(null=True, blank=True)
-    realname = models.CharField( max_length=40, null=True, blank=True)
-    email = models.EmailField(max_length=254, null=True, unique=True)
-    created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    nationality = models.CharField(max_length=50, null=True, blank=True)
-    campermode = models.CharField(max_length=30, choices=MODES, default="Hammock", null=True, blank=True)
-    camperstory = models.TextField(null=True, blank=True)
-    fav_campsite = models.CharField(max_length=50, null=True, blank=True)
-    level = models.CharField(max_length=30, choices=LEVELS, default="New SuperCamper", null=True, blank=True)
-    plot_reports = models.IntegerField(default=0, null=True, blank=True)
+    # cspell:ignore numplots
     numplots = models.IntegerField(default=0, null=True, blank=True)
-    
-    def __str__(self):
-        return str(self.user)
     
     @property
     def name(self):
@@ -62,7 +49,7 @@ class Profile(models.Model):
     def avatar(self):
         try:
             avatar = self.image.url
-        except:
+        except:  # noqa: E722
             avatar = static('images/avatar.svg')
         return avatar
 
