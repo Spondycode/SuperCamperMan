435055f8880a545ec31d89542699552e8858bc62
David Allen <spondicious@me.com>
2024-04-03 13:09:49 +0200

    Changed allowed hosts and hopefully turned off debug

--- a_core/settings.py
+++ a_core/settings.py
@@ -34,9 +34,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rj#-z^kx3j+1ay397otg6j8m_8#v^$^$jys6&41vy^&6le)ezc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://supercamper.onrender.com/', 'https://supercamper.app/']
 
 
 # Application definition
