d251b42094a603db7eebe56cbbde8fc6b04ef2d2
David Allen <spondicious@me.com>
2024-04-09 11:24:06 +0200

    It's got messed up - blog page no longe working - Weird

--- a_blog/urls.py
+++ a_blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import blog_view
+from .views import BlogView, ArticleDetailView, AddPostView
 
 
 urlpatterns = [
-    path('', blog_view, name="blog"),
+    # path('', blog_view, name="blog"),
+    path('', BlogView.as_view(), name="blog"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name="article-detail"),
+    path('add_post/', AddPostView.as_view(), name="add-post"),
 ]
